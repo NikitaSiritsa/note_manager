a237b7fe0e8b697ec5b89733eecaa5568863bb12
NikitaSiritsa <siritsa.nikita@gmail.com>
2025-01-12 22:28:12 +0500

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,16 @@
+username = "имя пользователя"
+title = "заголовок заметки"
+content = "описание заметки"
+status = "статус заметки"
+created_date =  "дата создания заметки"
+issue_date ="дата истечения заметки"
+print("имя пользователя",username,sep=", ")
+print("заголовок заметки",title,sep=", ")
+print("описание заметки",content,sep=", ")
+print("статус заметки", status,sep=", ")
+print("дата создания заметки",created_date,sep=", ")
+print("дата истечения заметки", issue_date,sep=", ")
+
+
+
+
